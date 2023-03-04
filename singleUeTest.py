c12b9f7ab2d769b2e3bc094347353063eb44a65a
Michael Angino <mfa7@rice.edu>
2023-03-03 20:58:44 -0600

    added subprocess plus called Iperf still not working though

--- singleUeTest.py
+++ singleUeTest.py
@@ -1,14 +1,19 @@
 import subprocess
 import time
 
+UE_IP_ADDRESS = "172.16.0.2" # I believe this is always the same 
+
 epc_call = subprocess.Popen(["sudo", "srsepc"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+enb_call = subprocess.Popen(["sudo", "srsenb"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-while(True):
-    time.sleep(1)
-    output, errors = epc_call.communicate(input="")
+# open file for writing
+with open("epc_output.txt", "w") as f1:
+    with open("enb_output.txt", "w") as f2:
+        while True:
+            output = epc_call.stdout.readline()
+            f1.write(output)
+            f1.flush()  # flush output to file
 
-    print(output)
-    print(errors)
-    
-
-
+            output = enb_call.stdout.readline()
+            f2.write(output)
+            f2.flush()  # flush output to file
